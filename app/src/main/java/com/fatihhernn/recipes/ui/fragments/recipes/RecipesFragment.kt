f94e1ca16dfaecff4a6d1675eb2e329f8a2ab608
fatih eren <fatihhernn@gmail.com>
2021-07-21 13:19:47 +0300

    Customize Chip Style

--- app/src/main/java/com/fatihhernn/recipes/ui/fragments/recipes/RecipesFragment.kt
+++ app/src/main/java/com/fatihhernn/recipes/ui/fragments/recipes/RecipesFragment.kt
@@ -9,7 +9,9 @@ import android.view.ViewGroup
 import android.widget.Toast
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.lifecycleScope
+import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.fatihhernn.recipes.R
 import com.fatihhernn.recipes.viewmodels.MainViewModel
 import com.fatihhernn.recipes.adapters.RecipesAdapter
 import com.fatihhernn.recipes.databinding.FragmentRecipesBinding
@@ -47,6 +49,10 @@ class RecipesFragment : Fragment() {
 
         setupRecyclerView()
         readDatabase()
+
+        binding.recipesFab.setOnClickListener {
+            findNavController().navigate(R.id.action_recipesFragment_to_recipesBottomSheet)
+        }
 
         return binding.root
     }
