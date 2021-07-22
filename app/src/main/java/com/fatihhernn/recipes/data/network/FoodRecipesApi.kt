b550f8d23445486087ac5af3aab5bf36563104db
fatih eren <fatihhernn@gmail.com>
2021-07-22 14:41:06 +0300

    FoodRecipesApi-searchRecipes()

--- app/src/main/java/com/fatihhernn/recipes/data/network/FoodRecipesApi.kt
+++ app/src/main/java/com/fatihhernn/recipes/data/network/FoodRecipesApi.kt
@@ -3,12 +3,20 @@ package com.fatihhernn.recipes.data.network
 import com.fatihhernn.recipes.models.FoodRecipe
 import retrofit2.Response
 import retrofit2.http.GET
+import retrofit2.http.Query
 import retrofit2.http.QueryMap
 
 interface FoodRecipesApi {
+//https://api.spoonacular.com/recipes/complexSearch?number=50&apiKey=5f4fae60dba24eee90ffcb754c28259a&type=snack&diet=vegan&addRecipeInformation=true&fillIngredients=true
 
     @GET("/recipes/complexSearch")
     suspend fun getRecipes(
         @QueryMap queries:Map<String,String>
+    ):Response<FoodRecipe>
+
+
+    @GET("/recipes/complexSearch")
+    suspend fun searchRecipes(
+        @QueryMap searchQuery: Map<String,String>
     ):Response<FoodRecipe>
 }
