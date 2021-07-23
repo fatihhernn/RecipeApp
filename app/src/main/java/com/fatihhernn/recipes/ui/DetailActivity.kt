00c41cbced4c9405fb9bce38b0dd5370f23c47ce
fatih eren <fatihhernn@gmail.com>
2021-07-23 14:17:04 +0300

    Create PagerAdapter class

--- app/src/main/java/com/fatihhernn/recipes/ui/DetailActivity.kt
+++ app/src/main/java/com/fatihhernn/recipes/ui/DetailActivity.kt
@@ -1,19 +1,22 @@
 package com.fatihhernn.recipes.ui
 
-import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.AttributeSet
 import android.view.MenuItem
-import android.view.View
 import androidx.core.content.ContextCompat
+import androidx.fragment.app.Fragment
+import androidx.navigation.navArgs
 import com.fatihhernn.recipes.R
+import com.fatihhernn.recipes.adapters.PagerAdapter
 import com.fatihhernn.recipes.databinding.ActivityDetailBinding
-import com.fatihhernn.recipes.databinding.FragmentRecipesBinding
+import com.fatihhernn.recipes.ui.fragments.ingredients.IngredientsFragment
+import com.fatihhernn.recipes.ui.fragments.instructions.InstructionsFragment
+import com.fatihhernn.recipes.ui.fragments.overview.OverviewFragment
 
 class DetailActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityDetailBinding
+    private val args by navArgs<DetailActivityArgs>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,11 +24,26 @@ class DetailActivity : AppCompatActivity() {
         val view = binding.root
         setContentView(view)
 
-
-
         setSupportActionBar(binding.toolBar)
         binding.toolBar.setTitleTextColor(ContextCompat.getColor(this,R.color.white))
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        val fragments=ArrayList<Fragment>()
+        fragments.add(OverviewFragment())
+        fragments.add(IngredientsFragment())
+        fragments.add(InstructionsFragment())
+
+        val titles=ArrayList<String>()
+        titles.add("Overview")
+        titles.add("Ingredients")
+        titles.add("Instructions")
+
+        val resultBundle=Bundle()
+        resultBundle.putParcelable("recipeBundle",args.result)
+
+        val adapter=PagerAdapter(resultBundle,fragments,titles,supportFragmentManager)
+        binding.viewPager.adapter=adapter
+        binding.tabLayout.setupWithViewPager(binding.viewPager)
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
