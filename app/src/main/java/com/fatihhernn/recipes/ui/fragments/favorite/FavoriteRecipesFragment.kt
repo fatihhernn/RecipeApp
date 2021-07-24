03a545a695a79c21081eae4d35e9caf694932006
fatih eren <fatihhernn@gmail.com>
2021-07-24 22:29:56 +0300

    Create FavoriteRecipesBinding class - Visible Invisible Screen w.r.t Data

--- app/src/main/java/com/fatihhernn/recipes/ui/fragments/favorite/FavoriteRecipesFragment.kt
+++ app/src/main/java/com/fatihhernn/recipes/ui/fragments/favorite/FavoriteRecipesFragment.kt
@@ -5,18 +5,51 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.fatihhernn.recipes.R
+import com.fatihhernn.recipes.adapters.FavoriteRecipesAdapter
+import com.fatihhernn.recipes.databinding.FragmentFavoriteRecipesBinding
+import com.fatihhernn.recipes.viewmodels.MainViewModel
+import dagger.hilt.android.AndroidEntryPoint
 
-
+@AndroidEntryPoint
 class FavoriteRecipesFragment : Fragment() {
 
+    private val mAdapter: FavoriteRecipesAdapter by lazy { FavoriteRecipesAdapter() }
+    private val mainViewModel: MainViewModel by viewModels()
+
+    private var _binding:FragmentFavoriteRecipesBinding?=null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_favorite_recipes, container, false)
+        _binding= FragmentFavoriteRecipesBinding.inflate(inflater,container,false)
+        binding.lifecycleOwner=this
+        binding.mainViewModel=mainViewModel
+        binding.mAdapter = mAdapter
+
+        setUpRecyclerView(binding.favoriteRecipesRecyclerView)
+
+        /** Implemented in bindingAdapters->FavoriteRecipeBinding - if adapter is empty data it controls
+        mainViewModel.readFavoriteRecipes.observe(viewLifecycleOwner, { favoritesEntity ->
+            mAdapter.setData(favoritesEntity)
+        })
+         */
+
+        return binding.root
     }
 
+    private fun setUpRecyclerView(recyclerView: RecyclerView) {
+        recyclerView.adapter = mAdapter
+        recyclerView.layoutManager = LinearLayoutManager(requireContext())
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding=null
+    }
 }
