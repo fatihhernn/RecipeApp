a1ceb012b33a36a52cb6f3ecbef70d523e7d5224
fatih eren <fatihhernn@gmail.com>
2021-07-25 01:27:07 +0300

    FavoriteRecipesFragment - Long Click Listener

--- app/src/main/java/com/fatihhernn/recipes/adapters/FavoriteRecipesAdapter.kt
+++ app/src/main/java/com/fatihhernn/recipes/adapters/FavoriteRecipesAdapter.kt
@@ -1,59 +1,91 @@
 package com.fatihhernn.recipes.adapters
 
-import android.view.LayoutInflater
-import android.view.ViewGroup
-import androidx.constraintlayout.solver.state.State
-import androidx.constraintlayout.widget.ConstraintSet
+import android.view.*
+import androidx.fragment.app.FragmentActivity
 import androidx.navigation.findNavController
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
 import com.fatihhernn.recipes.R
 import com.fatihhernn.recipes.data.database.entities.FavoritesEntity
 import com.fatihhernn.recipes.databinding.FavoriteRecipesRowLayoutBinding
-import com.fatihhernn.recipes.databinding.FragmentRecipesBinding
 import com.fatihhernn.recipes.ui.fragments.favorite.FavoriteRecipesFragmentDirections
 import com.fatihhernn.recipes.util.RecipesDiffUtil
 
-class FavoriteRecipesAdapter : RecyclerView.Adapter<FavoriteRecipesAdapter.MyViewHolder>() {
+class FavoriteRecipesAdapter(
+    private val requireActivity: FragmentActivity
+) : RecyclerView.Adapter<FavoriteRecipesAdapter.MyViewHolder>(),
+    ActionMode.Callback {
 
-    private var favoriteRecipes= emptyList<FavoritesEntity>()
+    private var favoriteRecipes = emptyList<FavoritesEntity>()
 
     class MyViewHolder(val binding: FavoriteRecipesRowLayoutBinding) :
         RecyclerView.ViewHolder(binding.root) {
 
-            fun bind(favoritesEntity: FavoritesEntity){
-                binding.favoritesEntity=favoritesEntity
-                binding.executePendingBindings()
-            }
+        fun bind(favoritesEntity: FavoritesEntity) {
+            binding.favoritesEntity = favoritesEntity
+            binding.executePendingBindings()
+        }
 
-        companion object{
-            fun from(parent: ViewGroup):MyViewHolder{
-                val layoutInflater=LayoutInflater.from(parent.context)
-                val binding=FavoriteRecipesRowLayoutBinding.inflate(layoutInflater,parent,false)
+        companion object {
+            fun from(parent: ViewGroup): MyViewHolder {
+                val layoutInflater = LayoutInflater.from(parent.context)
+                val binding = FavoriteRecipesRowLayoutBinding.inflate(layoutInflater, parent, false)
                 return MyViewHolder(binding)
             }
         }
 
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder = MyViewHolder.from(parent)
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder =
+        MyViewHolder.from(parent)
 
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
-       val selectedRecipe=favoriteRecipes[position]
+        val selectedRecipe = favoriteRecipes[position]
         holder.bind(selectedRecipe)
 
+        /**
+         * .. Single Click Listener
+         * */
         holder.binding.favoriteRecipesRowLayout.setOnClickListener {
-            val action= FavoriteRecipesFragmentDirections.actionFavoriteRecipesFragmentToDetailsActivity(favoriteRecipes[position].result)
+            val action =
+                FavoriteRecipesFragmentDirections.actionFavoriteRecipesFragmentToDetailsActivity(
+                    favoriteRecipes[position].result
+                )
             holder.itemView.findNavController().navigate(action)
+        }
+
+        /**
+         * .. Long Click Listener
+         * */
+        holder.binding.favoriteRecipesRowLayout.setOnClickListener {
+            requireActivity.startActionMode(this)
+            true
         }
     }
 
-    override fun getItemCount(): Int =favoriteRecipes.size
+    override fun getItemCount(): Int = favoriteRecipes.size
 
-    fun setData(newFavoriteRecipes:List<FavoritesEntity>){
-        val favoriteRecipesDiffUtil=RecipesDiffUtil(favoriteRecipes,newFavoriteRecipes)
-        val diffUtilResult=DiffUtil.calculateDiff(favoriteRecipesDiffUtil)
-        favoriteRecipes=newFavoriteRecipes
+    fun setData(newFavoriteRecipes: List<FavoritesEntity>) {
+        val favoriteRecipesDiffUtil = RecipesDiffUtil(favoriteRecipes, newFavoriteRecipes)
+        val diffUtilResult = DiffUtil.calculateDiff(favoriteRecipesDiffUtil)
+        favoriteRecipes = newFavoriteRecipes
         diffUtilResult.dispatchUpdatesTo(this)
+    }
+
+    override fun onCreateActionMode(actionMode: ActionMode, menu: Menu?): Boolean {
+        actionMode.menuInflater.inflate(R.menu.favorites_contextual_menu, menu)
+        return true
+    }
+
+    override fun onPrepareActionMode(actionMode: ActionMode?, menu: Menu?): Boolean {
+        return true
+    }
+
+    override fun onActionItemClicked(actionMode: ActionMode?, item: MenuItem?): Boolean {
+        return true
+    }
+
+    override fun onDestroyActionMode(actionMode: ActionMode?) {
+
     }
 }
