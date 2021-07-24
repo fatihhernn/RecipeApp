242f26e051e0009369ca8f2a776bc36313eeda13
fatih eren <fatihhernn@gmail.com>
2021-07-25 01:06:23 +0300

    FavoriteRecipesFragment - Single Click Listener

--- app/src/main/java/com/fatihhernn/recipes/adapters/FavoriteRecipesAdapter.kt
+++ app/src/main/java/com/fatihhernn/recipes/adapters/FavoriteRecipesAdapter.kt
@@ -2,17 +2,23 @@ package com.fatihhernn.recipes.adapters
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.constraintlayout.solver.state.State
+import androidx.constraintlayout.widget.ConstraintSet
+import androidx.navigation.findNavController
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
+import com.fatihhernn.recipes.R
 import com.fatihhernn.recipes.data.database.entities.FavoritesEntity
 import com.fatihhernn.recipes.databinding.FavoriteRecipesRowLayoutBinding
+import com.fatihhernn.recipes.databinding.FragmentRecipesBinding
+import com.fatihhernn.recipes.ui.fragments.favorite.FavoriteRecipesFragmentDirections
 import com.fatihhernn.recipes.util.RecipesDiffUtil
 
 class FavoriteRecipesAdapter : RecyclerView.Adapter<FavoriteRecipesAdapter.MyViewHolder>() {
 
     private var favoriteRecipes= emptyList<FavoritesEntity>()
 
-    class MyViewHolder(private val binding: FavoriteRecipesRowLayoutBinding) :
+    class MyViewHolder(val binding: FavoriteRecipesRowLayoutBinding) :
         RecyclerView.ViewHolder(binding.root) {
 
             fun bind(favoritesEntity: FavoritesEntity){
@@ -35,6 +41,11 @@ class FavoriteRecipesAdapter : RecyclerView.Adapter<FavoriteRecipesAdapter.MyVie
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
        val selectedRecipe=favoriteRecipes[position]
         holder.bind(selectedRecipe)
+
+        holder.binding.favoriteRecipesRowLayout.setOnClickListener {
+            val action= FavoriteRecipesFragmentDirections.actionFavoriteRecipesFragmentToDetailsActivity(favoriteRecipes[position].result)
+            holder.itemView.findNavController().navigate(action)
+        }
     }
 
     override fun getItemCount(): Int =favoriteRecipes.size
