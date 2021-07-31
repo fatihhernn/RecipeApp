5b4da51bbeafc3362d14aee9f62400fffb31639f
fatih eren <fatihhernn@gmail.com>
2021-07-31 20:40:17 +0300

    add share menu

--- app/src/main/java/com/fatihhernn/recipes/ui/fragments/foodJoke/FoodJokeFragment.kt
+++ app/src/main/java/com/fatihhernn/recipes/ui/fragments/foodJoke/FoodJokeFragment.kt
@@ -1,14 +1,14 @@
 package com.fatihhernn.recipes.ui.fragments.foodJoke
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import com.fatihhernn.recipes.R
 
 class FoodJokeFragment : Fragment() {
 
+    private var foodJoke="No Food Joke"
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -16,6 +16,24 @@ class FoodJokeFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_food_joke, container, false)
+
+        setHasOptionsMenu(true)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.food_joke_menu,menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId==R.id.share_food_joke_menu){
+            val shareIntent=Intent().apply {
+                this.action=Intent.ACTION_SEND
+                this.putExtra(Intent.EXTRA_TEXT,foodJoke)
+                this.type="text/plain"
+            }
+            startActivity(shareIntent)
+        }
+        return super.onOptionsItemSelected(item)
     }
 
 }
